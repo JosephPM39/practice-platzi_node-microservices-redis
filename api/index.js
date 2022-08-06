a14952f5732f54e9cd9a0f5bc1f3defdbc93a425
JosephPM39 <josias.jjpm49@hotmail.com>
2022-08-06 13:49:20 -0600

    Using swagger to add api docs

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const config = require('../config.js');
 const user = require('./components/user/network');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDoc = require('./swagger.json');
 
 const app = express();
 
 app.use('/api/user', user);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
 	console.log('Api escuchando en el puerto ', config.api.port);
